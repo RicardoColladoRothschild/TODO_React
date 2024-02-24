2404610c5ac17b2bb86769ebb0040cc82d32b50d
ricardocollado <ricardocolladorothschild@gmail.com>
2024-02-24 10:51:09 -0400

    Components: list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './platzi.webp';
 import './App.css';
+import logo from './platzi.webp';
 
 function App() {
   return (
     <div className="App">
+      <h2>Has completado 4 de 5</h2>
+      <ul>
+      <TodoItem completed={2} total={5}/>
+      <TodoItem completed={3} total={5}/>
+      <TodoItem completed={4} total={5} />
+      </ul>
+     
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +23,26 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
   );
+}
+
+function TodoItem(props){
+    return (
+      <li>
+        <span>V</span>
+        <p>Mejorar en React</p>
+        <h1>
+          Completaste:
+            {props.completed} de
+            {props.total} TO-DOs
+        </h1>
+        <span>X</span>
+      </li>
+    );
 }
 
 export default App;
