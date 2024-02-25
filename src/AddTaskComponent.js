f515296caacc13071472b37a46ba1d8ed8d0c590
ricardocollado <ricardocolladorothschild@gmail.com>
2024-02-25 19:26:41 -0400

    providing style to addTaskContainer

--- src/AddTaskComponent.js
+++ src/AddTaskComponent.js
@@ -1,9 +1,15 @@
+import './AddTaskComponent.css';
 import { CreateTodoButton } from './CreateTodoButton';
+import { TaskImagen } from './TaskImagen';
 function AddTaskComponent(){
     return (
         <div>
+            <div className="newtask-container">
             <input placeholder="Comprar cafe"/>
              <CreateTodoButton/>
+            </div>
+            
+             <TaskImagen/>
         </div>
         
     );
---
+++ src/TaskImagen.js
@@ -0,0 +1,11 @@
+import office from './assets/office.png';
+import './TaskImagen.css';
+function TaskImagen(){
+    return (
+        <div className="image-container">
+            <img src={office} alt={""}></img>
+        </div>
+    );
+}
+
+export { TaskImagen };
