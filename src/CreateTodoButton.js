860b93e56a79b8959cc10b7778d7df9d9b208170
ricardocollado <ricardocolladorothschild@gmail.com>
2024-02-27 23:32:44 -0400

    Dealing with events for add button and todosearch

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,8 +1,15 @@
 import './Button.css';
 function CreateTodoButton(){
     return (
-        <button>
-            +
+        <button className="createTodoButton"
+        onClick={
+            (event)=>{
+                  
+                    console.log(event);
+                    console.log("Has hecho click en agregar, y esto esta en el componente CreateTodoButton");
+                }
+        }
+        >+
         </button>
     );
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,17 @@
 import './TodoSearch.css';
 function TodoSearch(){
     return (
-        <input placeholder = "clases de TypeScript" className="TodoSearch"/>
+        <input placeholder = "clases de TypeScript" 
+        className="TodoSearch"
+        onChange={(event)=>{
+                console.log('Estas escribiendo en el todo, search');
+                console.log(event);
+                console.log(event.target);
+                console.log(event.target.value);
+
+ 
+        }
+    }/>
     );
 }
 
