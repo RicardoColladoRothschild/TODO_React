f4163cd879b62959a477b54cbea974991d6547b0
ricardocollado <ricardocolladorothschild@gmail.com>
2024-03-09 17:19:05 -0400

    create useLocalStorage custom hook

--- src/App.js
+++ src/App.js
@@ -6,21 +6,41 @@ import { MainComponent } from './MainComponent';
 import { SubComponent } from './SubComponent';
 import { TodoCounter } from './TodoCounter';
 
-//localStorage.setItem('TODOListRick_V1', defaultTodos);
-function App() {
-  const localStorageTodos = localStorage.getItem('TODOListRick_V1');
-  let parsedTodos;
-    if(!localStorageTodos){
-      localStorage.setItem('TODOListRick_V1', JSON.stringify([]));
-      parsedTodos=[];
+//'TODOListRick_V1'
+function useLocalStorage(itemName, initialValue){
+    
+
+  const localStorageItem = localStorage.getItem(itemName);
+  
+  let parsedItem;
+
+
+    if(!localStorageItem){
+      localStorage.setItem(itemName, JSON.stringify(initialValue));
+      parsedItem=initialValue;
     }else{
 
-      parsedTodos = JSON.parse(localStorageTodos);
+      parsedItem = JSON.parse(localStorageItem);
     }
 
+    
+
+    const [item, setItem] = React.useState(parsedItem);
+
+    const saveItem = (newItem) =>{
+      localStorage.setItem(itemName, JSON.stringify(newItem));
+      setItem(newItem);
+   };
+
+    return[item, saveItem];
+}
+
+
+//localStorage.setItem('TODOListRick_V1', defaultTodos);
+function App() {
    
 
-  const [todos, setTodos] = React.useState(parsedTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOListRick_V1', []);
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -31,7 +51,7 @@ function App() {
          <TodoCounter completed = {completedTodos} total = {totalTodos}/>
 
          <MainComponent>
-          <SubComponent todos = {todos} setTodos = {setTodos}/>
+          <SubComponent todos = {todos} setTodos = {saveTodos}/>
           
 
          </MainComponent>
