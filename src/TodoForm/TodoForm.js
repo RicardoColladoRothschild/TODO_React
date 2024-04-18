8a92d415eec0de03cf28c1f693b077a7dff809a2
ricardocollado <ricardocolladorothschild@gmail.com>
2024-04-18 00:08:05 -0400

    complete add form style

--- src/TodoForm/TodoForm.js
+++ src/TodoForm/TodoForm.js
@@ -4,7 +4,7 @@ export const TodoForm = () =>{
 
 return(
         <div className="add-newtodo-container">
-            <label>Add a Todo</label>
+            <label className="label-newtodo--container">Add a Todo</label>
             <input className="new-todo-form"/>
             <div className="button-box-container">
                 <button className="btn-box-container btn-add--newtask-container">add</button>
