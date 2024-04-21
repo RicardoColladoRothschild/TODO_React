425cceca0139bd472b93b48fd1d469cb2692c40c
ricardocollado <ricardocolladorothschild@gmail.com>
2024-04-21 18:09:47 -0400

    finish modal portal process

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import './Modal.css';
 
 function Modal({children}){
 
     return ReactDom.createPortal(
         <div className="ModalBackground">
+            <div className="modal">
             {children}
+            </div>
         </div>, 
             document.getElementById('modal')
     );
