7bd690d881294ac733922db43d85a950febda2d9
ricardocollado <ricardocolladorothschild@gmail.com>
2024-04-21 11:27:43 -0400

    working with modals

--- src/CreateTodoButton/CreateTodoButton.js
+++ src/CreateTodoButton/CreateTodoButton.js
@@ -2,19 +2,12 @@ import './Button.css';
 function CreateTodoButton({setOpenModal, openModal}){
 
 
-    function createTodoEventHandle(event){
-            
-        if(!openModal){
-            setOpenModal(true);
-            
-        }else{
-            setOpenModal(false);
-        }
-        
-    }
+    
     return (
         <button className="createTodoButton"
-        onClick={createTodoEventHandle}
+        onClick={()=>{
+            setOpenModal(state=>!state);
+        }}
         >+
         </button>
     );
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -5,7 +5,7 @@ import ReactDom from 'react-dom';
 function Modal({children}){
 
     return ReactDom.createPortal(
-        <div className="Modal">
+        <div className="ModalBackground">
             {children}
         </div>, 
             document.getElementById('modal')
